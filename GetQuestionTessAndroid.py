eb7dcc09e468290ee7fafe6ccd4269a71e386802
Han YUN <han.yun@gmail.com>
2018-01-16 00:22:36 +0800

    add choices search

--- GetQuestionTessAndroid.py
+++ GetQuestionTessAndroid.py
@@ -14,16 +14,17 @@ import time
 DEBUG_SWITCH = True
 
 while True:
+    print("=========================================")  
     t = time.clock()
     # 截图
-    
-    #screenshot.check_screenshot()
+
+    # screenshot.check_screenshot()
 
     img = Image.open("./screenshot.png")
 
     # 文字识别
     question, choices = ocr.ocr_img(img)
- 
+
     # 用不同方法输出结果，取消某个方法在前面加上#
 
     # # 打开浏览器方法搜索问题
@@ -33,21 +34,28 @@ while True:
     # # 用选项在问题页面中计数出现词频方法
     # methods.run_algorithm(2, question, choices)
 
+    if DEBUG_SWITCH:
+        print("OCR time: ",time.clock()-t)
     # 多线程
     #m1 = Thread(methods.run_algorithm(0, question, choices))
     #m2 = Thread(methods.run_algorithm(1, question, choices))
     m3 = Thread(methods.run_algorithm(2, question, choices))
-    #m1.start()
-    #m2.start()
+    # m1.start()
+    # m2.start()
     m3.start()
 
     end_time = time.clock()
     print(end_time - t)
     if DEBUG_SWITCH:
-        ts=time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+        ts = "screenshot_"+time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
         debug.backup_screenshot(ts)
-    go = input('输入回车继续运行,输入 n 回车结束运行: ')
+    go = input('输入回车继续运行,输入 n 回车结束运行, 输入 m 搜索答案: ')
     if go == 'n':
         break
+    elif go == 'm':
+        methods.search_choices(choices)
+        print('------------------------')
+    input('输入回车继续运行')
 
-    print('------------------------')
+
+ 
--- common/methods.py
+++ common/methods.py
@@ -10,13 +10,15 @@ import urllib.parse
 from . import process
 
 # # 颜色兼容Win 10
-from colorama import init,Fore
+from colorama import init, Fore
 init()
+
 
 def open_webbrowser(question):
     webbrowser.open('https://baidu.com/s?wd=' + urllib.parse.quote(question))
 
-def open_webbrowser_count(question,choices):
+
+def open_webbrowser_count(question, choices):
     print('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
     print('Question: ' + question)
     if '不是' in question:
@@ -25,7 +27,8 @@ def open_webbrowser_count(question,choices):
     counts = []
     for i in range(len(choices)):
         # 请求
-        req = requests.get(url='http://www.baidu.com/s', params={'wd': question + choices[i]})
+        req = requests.get(url='http://www.baidu.com/s',
+                           params={'wd': question + choices[i]})
         content = req.text
         index = content.find('百度为您找到相关结果约') + 11
         content = content[index:]
@@ -35,36 +38,39 @@ def open_webbrowser_count(question,choices):
         #print(choices[i] + " : " + count)
     output(choices, counts)
 
-def count_base(question,choices):
+
+def count_base(question, choices):
     print('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
     # 请求
-    req = requests.get(url='http://www.baidu.com/s', params={'wd':question})
+    req = requests.get(url='http://www.baidu.com/s', params={'wd': question})
     content = req.text
-    #print(content)
-    counts = []
+    # print(content)
 
     print("====================\r\n")
-    results=process.page(content)
+    results = process.page(content)
     #print (results)
     count = 0
     for result in results:
-        result_str=('{0}'.format(result.abstract))
+        result_str = ('{0}'.format(result.abstract))
         if "当前代码版本不予摘要" in result_str:
             continue
-        else:            
-            #print('{0} {1} {2} {3} {4}'.format(result.index, result.title, result.abstract, result.show_url, result.url))  # 此处应有格式化输出
-            print(result_str+"\r\n")  # 此处应有格式化输出
-            count=count+1
-            if(count == 5):      #这里限制了只显示2条结果，可以自己设置
+        else:
+            # print('{0} {1} {2} {3} {4}'.format(result.index, result.title, result.abstract, result.show_url, result.url))  # 此处应有格式化输出
+            print(result_str + "\r\n")  # 此处应有格式化输出
+            count = count + 1
+            if(count == 5):  # 这里限制了只显示2条结果，可以自己设置
                 break
     print("====================\r\n")
-    print('Question: '+question)
+    print('Question: ' + question)
     if '不是' in question:
         print('**请注意此题为否定题,选计数最少的**')
+    counts = []
+
     for i in range(len(choices)):
         counts.append(content.count(choices[i]))
         #print(choices[i] + " : " + str(counts[i]))
     output(choices, counts)
+
 
 def output(choices, counts):
     counts = list(map(int, counts))
@@ -76,20 +82,46 @@ def output(choices, counts):
     # 计数最少
     index_min = counts.index(min(counts))
 
-    if index_max == index_min:
-        print(Fore.RED + "高低计数相等此方法失效！" + Fore.RESET)
-        return
-
     for i in range(len(choices)):
         print()
         if i == index_max:
             # 绿色为计数最高的答案
-            print(Fore.GREEN + "{0} : {1} ".format(choices[i], counts[i]) + Fore.RESET)
+            print(Fore.GREEN +
+                  "{0} : {1} ".format(choices[i], counts[i]) + Fore.RESET)
         elif i == index_min:
             # 红色为计数最低的答案
-            print(Fore.MAGENTA + "{0} : {1}".format(choices[i], counts[i]) + Fore.RESET)
+            print(Fore.MAGENTA +
+                  "{0} : {1}".format(choices[i], counts[i]) + Fore.RESET)
         else:
             print("{0} : {1}".format(choices[i], counts[i]))
+            
+    if index_max == index_min:
+        print(Fore.RED + "高低计数相等此方法失效！" + Fore.RESET)
+        return
+
+
+def search_choices(choices):
+
+    for choice in choices:
+        req = requests.get(url='http://www.baidu.com/s', params={'wd': choice})
+        content = req.text
+        # print(content)
+        #counts = []
+
+        print("====================\r\n")
+        results = process.page(content)
+        #print (results)
+        count = 0
+        for result in results:
+            result_str = ('{0}'.format(result.abstract))
+            if "当前代码版本不予摘要" in result_str:
+                continue
+            else:
+                # print('{0} {1} {2} {3} {4}'.format(result.index, result.title, result.abstract, result.show_url, result.url))  # 此处应有格式化输出
+                print(result_str + "\r\n")  # 此处应有格式化输出
+                count = count + 1
+                if(count == 2):  # 这里限制了只显示2条结果，可以自己设置
+                    break
 
 
 def run_algorithm(al_num, question, choices):
@@ -100,9 +132,8 @@ def run_algorithm(al_num, question, choices):
     elif al_num == 2:
         count_base(question, choices)
 
+
 if __name__ == '__main__':
     question = '新装修的房子通常哪种化学物质含量会比较高?'
     choices = ['甲醛', '苯', '甲醇']
     run_algorithm(1, question, choices)
-
-
